fc2ffea081f14989a7de7e8fba9bb729bea02014
CALedesma87 <Alejandro_ledesma_87@live.com.ar>
2023-09-07 21:30:04 -0300

    Modificado

--- src/universidadgrupo37/UniversidadGrupo37.java
+++ src/universidadgrupo37/UniversidadGrupo37.java
@@ -1,12 +1,17 @@
 
 package universidadgrupo37;
 
+import java.sql.Connection;
+import universidadgrupo37.accesoADatos.Conexion;
+
 
 public class UniversidadGrupo37 {
 
  
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Connection con=Conexion.getConexion();
     }
     
 }
---
+++ src/universidadgrupo37/accesoADatos/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgrupo37.accesoADatos;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Conexion {
+
+    private static final String URL ="jdbc:mariadb://localhost/";
+    private static final String DB ="universidadulp";
+    private static final String USUARIO ="root";
+    private static final String PASSWORD ="";
+    private static Connection conexion;
+
+    public Conexion() {
+    }
+
+    public static Connection getConexion() {
+
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                
+                JOptionPane.showMessageDialog(null, "Conexion exitosa!!");
+                
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "No se cargo el Driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "No se pudo establecer conexion a la Base de Datos");
+            }
+
+        }
+        return conexion;
+
+    }
+}
