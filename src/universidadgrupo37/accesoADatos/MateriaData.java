63f4f4b3a12200515f94a49c84f5de5a3ce2a44a
CALedesma87 <Alejandro_ledesma_87@live.com.ar>
2023-09-11 22:38:36 -0300

    volvi a agregar lo que estaba escrito en MateriaData

--- src/universidadgrupo37/accesoADatos/MateriaData.java
+++ src/universidadgrupo37/accesoADatos/MateriaData.java
@@ -1,7 +1,30 @@
 
 package universidadgrupo37.accesoADatos;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import universidadgrupo37.entidades.Materia;
+
 
 public class MateriaData {
-    
+    private Connection con = null;
+
+    public MateriaData() {
+        con = Conexion.getConexion();
+    }
+
+    public void guardarMateria(Materia materia){}
+
+    //Materia buscarMateria(int id){}
+
+    public void modificarMateria(Materia materia){}
+
+    public void eliminarMateria(int id){}
+
+    public List<Materia> listarMaterias(int id){
+        List <Materia> materias = new ArrayList<Materia>();
+
+
+    }
 }
